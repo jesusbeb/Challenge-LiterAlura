2e10f2600348505597582d136d338ef21bbe5bbc
jesusbeb <jesusbelb@gmail.com>
2024-05-22 16:54:28 -0600

    Creando el metodo para buscar libro en la Api

--- src/main/java/com/alura/literalura/principal/Principal.java
+++ src/main/java/com/alura/literalura/principal/Principal.java
@@ -2,9 +2,12 @@ package com.alura.literalura.principal;
 
 import com.alura.literalura.model.DatosAutor;
 import com.alura.literalura.model.JsonResults;
+import com.alura.literalura.model.DatosLibro;
 import com.alura.literalura.service.ConsumoApi;
 import com.alura.literalura.service.ConvierteDatos;
+import com.alura.literalura.service.LibroService;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Principal {
@@ -14,73 +17,71 @@ public class Principal {
     private ConsumoApi consumoApi = new ConsumoApi();
     private final String URL_BASE = "https://gutendex.com/books/";
     private ConvierteDatos convierteDatos = new ConvierteDatos();
-
+    private LibroService libroService = new LibroService();
 
 
     //Metodo que muestra el menu al usuario
     public void muestraMenu(){
 
-        //Usamos el metodo para consumir la Api y la almacenamos en un String
-        var json = consumoApi.obtenerDatosApi(URL_BASE);
-        //System.out.println(json);
-        //Usamos el metodo para convertir el json en String a una clase que contiene un List<Libro> e
-        //imprimimos la lista con todos los libros y sus atributos
-        var datos = convierteDatos.JsonToClass(json, JsonResults.class);
-        System.out.println(datos);
+//        //Usamos el metodo para consumir la Api y la almacenamos en un String
+//        var json = consumoApi.obtenerDatosApi(URL_BASE);
+//        //System.out.println(json);
+//        //Usamos el metodo para convertir el json en String a una clase que contiene un List<Libro> e
+//        //imprimimos la lista con todos los libros y sus atributos
+//        //var datos = convierteDatos.JsonToClass(json, JsonResults.class);
+//        //System.out.println(datos);
 
+        do {
+            System.out.println("""
+                \nTeclea el numero de la opción deseada:
+                1. Buscar libro por título
+                2. Listar libros registrados
+                3. Listar autores registrados
+                4. Listar autores vivos en un determinado año
+                5. Listar libros por idioma
+                0. Salir
+                """);
+            opcion = teclado.nextInt();
+            teclado.nextLine(); //se coloca para evitar error
 
-
-
-
-
-//        do {
-//            System.out.println("""
-//                Teclea el numero de la opción deseada:
-//                1. Buscar libro por título
-//                2. Listar libros registrados
-//                3. Listar autores registrados
-//                4. Listar autores vivos en un determinado año
-//                5. Listar libros por idioma
-//                0. Salir
-//                """);
-//            opcion = teclado.nextInt();
-//
-//            switch (opcion){
-//                case 1:
-//                    System.out.println("Introduce el título del libro a buscar: ");
-//                    //buscarLibroPortitulo()
-//                    break;
-//                case 2:
-//                    System.out.println("LISTA DE LIBROS REGISTRADOS: ");
-//                    //listarLibrosRegistrados()
-//                    break;
-//                case 3:
-//                    System.out.println("LISTA DE AUTORES REGISTRADOS: ");
-//                    //listarAutoresRegistrados()
-//                    break;
-//                case 4:
-//                    System.out.println("Introduce el año para mostrar los autores vivos en ese año: ");
-//                    var year = teclado.nextInt();
-//                    //autoresVivos();
-//                    break;
-//                case 5:
-//                    System.out.println("""
-//                        Ingrese el idioma para buscar los libros:
-//                        es - español
-//                        en - inglés
-//                        fr - francés
-//                        pt - portugués
-//                        """);
-//                    var idioma = teclado.next();
-//                    teclado.nextLine();
-//                    //listarLibrosPorIdioma();
-//                case 0:
-//                    System.out.println("Saliendo de la aplicación... Hasta luego!");
-//                    break;
-//                default:
-//                    System.out.println("Opción inválida.");
-//            }
-//        } while (opcion != 0);
+            switch (opcion){
+                case 1:
+                    System.out.println("Introduce el titulo completo o parte del libro a buscar: ");
+                    var buscarLibro = teclado.nextLine();
+                    libroService.buscarLibroPortitulo(buscarLibro);
+                    break;
+                case 2:
+                    System.out.println("LISTA DE LIBROS REGISTRADOS: ");
+                    //listarLibrosRegistrados()
+                    break;
+                case 3:
+                    System.out.println("LISTA DE AUTORES REGISTRADOS: ");
+                    //listarAutoresRegistrados()
+                    break;
+                case 4:
+                    System.out.println("Introduce el año para mostrar los autores vivos en ese año: ");
+                    var year = teclado.nextInt();
+                    //autoresVivos();
+                    break;
+                case 5:
+                    System.out.println("""
+                        Ingrese el idioma para buscar los libros:
+                        es - español
+                        en - inglés
+                        fr - francés
+                        pt - portugués
+                        """);
+                    var idioma = teclado.next();
+                    teclado.nextLine();
+                    //listarLibrosPorIdioma();
+                    break;
+                case 0:
+                    System.out.println("Saliendo de la aplicación... Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opción inválida.");
+            }
+        } while (opcion != 0);
     }
 
 
---
+++ src/main/java/com/alura/literalura/service/LibroService.java
@@ -0,0 +1,43 @@
+package com.alura.literalura.service;
+
+import com.alura.literalura.model.DatosLibro;
+import com.alura.literalura.model.JsonResults;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+//Service para indicar que es una clase de servicio y trabajar con las reglas de
+//negocio como buena practica. El pack service hace la transformacion de datos,
+//operaciones, llamar al repository, contener los metodos, etc.
+@Service
+public class LibroService {
+    //variables e instancias
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private final String URL_BASE = "https://gutendex.com/books/";
+    private ConvierteDatos convierteDatos = new ConvierteDatos();
+
+
+    //Metodo para buscar libro en la Api
+    //Agregamos los parametros indicados a la URL_BASE que se envia al metodo obtenerDatosApi para poder buscar libros.
+    //Guardamos los resultados de la busqueda en la Api en datosBusqueda
+    //Creamos la variable local Optional de tipo DatosLibro que es el contenedor que almacenara el resultado si es encontrado o no.
+    //datosbusqueda es de tipo JsonResults, accesamos a la lista que tiene de libros y aplicamos stream
+    //filtramos la lista de libros para que solo contenga los que coincidan con el libro buscado por el usuario
+    //find.First obtiene el primer resultado. Se trata la variable Optional con if-else
+    public JsonResults buscarLibroPortitulo(String buscarLibro){
+        var json = consumoApi.obtenerDatosApi(URL_BASE + "?search" + buscarLibro.replace(" ", "%20"));
+        var datosBusqueda = convierteDatos.JsonToClass(json, JsonResults.class);
+        Optional<DatosLibro> libroBuscado = datosBusqueda.libro().stream()
+                .filter(l -> l.titulo().toUpperCase().contains(buscarLibro.toUpperCase()))
+                .findFirst();
+        if(libroBuscado.isPresent()){
+            System.out.println("Libro encontrado: " +libroBuscado.get());
+        } else {
+            System.out.println("Libro no encontrado.");
+        }
+        return datosBusqueda;
+    }
+
+
+
+}
