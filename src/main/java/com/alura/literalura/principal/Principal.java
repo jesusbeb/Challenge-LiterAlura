c9357535c80c3e6f0796777922fe775bc9fb1a01
jesusbeb <jesusbelb@gmail.com>
2024-05-28 16:16:20 -0600

    Se crea nuevamente el metodo buscarLibroPorTitulo() de la opcion 1 del menu de usuario, quedando funcional esa opcion

--- src/main/java/com/alura/literalura/principal/Principal.java
+++ src/main/java/com/alura/literalura/principal/Principal.java
@@ -1,36 +1,34 @@
 package com.alura.literalura.principal;
 
-import com.alura.literalura.model.DatosAutor;
-import com.alura.literalura.model.JsonResults;
-import com.alura.literalura.model.DatosLibro;
+import com.alura.literalura.model.*;
+import com.alura.literalura.repository.IAutorRepository;
+import com.alura.literalura.repository.ILibroRepository;
 import com.alura.literalura.service.ConsumoApi;
 import com.alura.literalura.service.ConvierteDatos;
-import com.alura.literalura.service.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Optional;
 import java.util.Scanner;
 
-public class Principal {
+public class Principal{
     //variables e instancias
     private int opcion = -1;
     private Scanner teclado = new Scanner(System.in);
     private ConsumoApi consumoApi = new ConsumoApi();
     private final String URL_BASE = "https://gutendex.com/books/";
     private ConvierteDatos convierteDatos = new ConvierteDatos();
-    private LibroService libroService = new LibroService();
+    @Autowired
+    private ILibroRepository libroRepo;
+    @Autowired
+    private IAutorRepository autorRepo;
 
+    //Construtor que recibe las interfaces inyectadas. Creamos atributos de las interfaces inyectadas
+    public Principal(ILibroRepository libroRepository, IAutorRepository autorRepository){
+        this.libroRepo = libroRepository;
+        this.autorRepo = autorRepository;
+    }
 
     //Metodo que muestra el menu al usuario
     public void muestraMenu(){
-
-//        //Usamos el metodo para consumir la Api y la almacenamos en un String
-//        var json = consumoApi.obtenerDatosApi(URL_BASE);
-//        //System.out.println(json);
-//        //Usamos el metodo para convertir el json en String a una clase que contiene un List<Libro> e
-//        //imprimimos la lista con todos los libros y sus atributos
-//        //var datos = convierteDatos.JsonToClass(json, JsonResults.class);
-//        //System.out.println(datos);
-
         do {
             System.out.println("""
                 \nTeclea el numero de la opción deseada:
@@ -46,9 +44,7 @@ public class Principal {
 
             switch (opcion){
                 case 1:
-                    System.out.println("Introduce el titulo completo o parte del libro a buscar: ");
-                    var buscarLibro = teclado.nextLine();
-                    libroService.buscarLibroPortitulo(buscarLibro);
+                    buscarLibroPortitulo();
                     break;
                 case 2:
                     System.out.println("LISTA DE LIBROS REGISTRADOS: ");
@@ -82,6 +78,63 @@ public class Principal {
                     System.out.println("Opción inválida.");
             }
         } while (opcion != 0);
+    }
+
+    //Metodo para buscar libro en la Api
+    //Agregamos los parametros indicados a la URL_BASE que se envia al metodo obtenerDatosApi para poder buscar libros.
+    //Los resultados que coinciden con las palabras del libro a buscar se guardan en la variable json
+    //Despues se guardan todos los libros encontrados, en una clase de tipo JsonResults que contiene un List<DatosLibro>
+    private JsonResults buscarLibroApi() {
+        System.out.println("Introduce el titulo completo o parte del libro a buscar: ");
+        var buscarLibro = teclado.nextLine();
+        System.out.println("Buscando libro, espere...");
+        String json = consumoApi.obtenerDatosApi(URL_BASE + "?search=" + buscarLibro.replace(" ", "%20"));
+        JsonResults librosEncontradosApi = convierteDatos.JsonToClass(json, JsonResults.class);
+        return librosEncontradosApi;
+    }
+
+    //En un objeto de tipo JsonResults se guarda la lista de libros encontrados del metodo buscarLibroApi()
+    //if librosEncontrdosApi es diferente de vacio, se instancia un objeto de DatosLibro y se obtiene el primer libro de la lista
+    //se instancia un objeto de tipo DatosAutor y se obtiene el primer autor de datosLibro
+    //Se instancia un objeto de tipo Libro para almacenar una busqueda por titulo de libro en la BD usando el metodo de ILibroRepository
+    //if libroDb existe, se imprime el libro de la BD
+    //else libroDb esta vacio, se instancia un objeto de Autor para almacenar una busqueda por nombre de Autor en la BD
+    //if autorDb esta vacio, se instancia objeto de Autor y se crea con los datosAutor, se guarda en la BD usando el metodo save del repositorio
+    //Se instancia tambien objeto de Libro y se crea con datosLibro y objeto Autor
+    //else autorDb existe, se instancia solamente objeto de Libro y se crea con datosLibro y autorDb
+    private void buscarLibroPortitulo() {
+        JsonResults librosEncontradosApi = buscarLibroApi();
+        if( !librosEncontradosApi.libro().isEmpty() ){
+            DatosLibro datosLibro = librosEncontradosApi.libro().get(0);
+            DatosAutor datosAutor = datosLibro.autor().get(0);
+
+            Libro libroDb = libroRepo.findByTitulo(datosLibro.titulo());
+            if(libroDb != null){
+                System.out.println("LIBRO ENCONTRADO EN LA BD: ");
+                System.out.println(libroDb);
+            }
+            else{
+                Autor autorDb = autorRepo.findByNombre(datosAutor.nombre());
+                if(autorDb == null){
+                    Autor autor = new Autor(datosAutor);
+                    autorRepo.save(autor);
+                    Libro libro = new Libro(datosLibro, autor);
+                    libroRepo.save(libro);
+                    System.out.println("LIBRO ENCONTRADO EN LA API: ");
+                    System.out.println(libro);
+                }
+                else{
+                    Libro libro = new Libro(datosLibro, autorDb);
+                    libroRepo.save(libro);
+                    System.out.println("LIBRO ENCONTRADO EN LA API: ");
+                    System.out.println(libro);
+                }
+            }
+
+        }
+        else{
+            System.out.println("Libro no encontrado :(");
+        }
     }
 
 
