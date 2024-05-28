5d889ea3c14038a38a3cd5a558d12fcab70f2b48
jesusbeb <jesusbelb@gmail.com>
2024-05-28 14:09:45 -0600

    Modificando los record DatosLibro y DatosAutor

--- src/main/java/com/alura/literalura/model/DatosAutor.java
+++ src/main/java/com/alura/literalura/model/DatosAutor.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DatosAutores(
+public record DatosAutor(
         @JsonAlias("name") String nombre,
         @JsonAlias("birth_year") int anioNacimiento,
         @JsonAlias("death_year") int anioDefuncion
--- src/main/java/com/alura/literalura/model/DatosLibro.java
+++ src/main/java/com/alura/literalura/model/DatosLibro.java
@@ -7,12 +7,13 @@ import java.util.List;
 
 //Creamos clase record de Datoslibro para obtener los datos del libro deseado y que estan dentro de la clave results del json
 //authors lo almacenamos en una lista, ya que en el json esta igual en formato de lista. La lista sera
-//de tipo DatosAutor y se creara la clase para mapear los datos que traiga
-//languages tambien esta en una lista, pero se creara de String, ya que momentaneamente no se mapearan sus datos
+//de tipo DatosAutor y se creara dicha clase para mapear los datos que traiga
+//languages tambien esta en una lista, pero se creara de String, ya que no se mapearan sus datos
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosLibro(
         @JsonAlias("title") String titulo,
         @JsonAlias("authors") List<DatosAutor> autor,
-        @JsonAlias("languages") List<String> idiomas
+        @JsonAlias("languages") List<String> idiomas,
+        @JsonAlias("download_count") int descargas
 ){
 }
