2d248007b14075c55e30760420f22fc2ec3db2b0
jesusbeb <jesusbelb@gmail.com>
2024-05-28 13:46:11 -0600

    Modificando la Clase o Entidad Libro

--- src/main/java/com/alura/literalura/model/Libro.java
+++ src/main/java/com/alura/literalura/model/Libro.java
@@ -4,15 +4,13 @@ import jakarta.persistence.*;
 
 import java.util.List;
 
-//Se agrega en el pom las dependencias de  Spring Data JPA y PostgreSQL Driver
+//Se agrega en el pom las dependencias de Spring Data JPA y PostgreSQL Driver
 //Utilizamos anotaciones de hibernate para mapear las entidades
 //Entity indica a JPA que la clase sera una tabla en la BD
 //Table indica el nombre que tendra la tabla en la BD
 //Id para indicar un atributo que sera el identificador en la tabla de la BD
 //GeneratedValue se indica la estrategia de generacion del id. IDENTITY indica que sera incremental y JPA se encargara de eso
 //@Transient indica a JPA que por el momento ignore el atributo y no cree la columna
-//Enumerated se le indica a JPA que es enum, una lista de otros atributos. Hay dos opciones: ORDINAL toma cada atributo del
-//enum por su posicion y STRING toma todos los atributos como un solo texto
 @Entity
 @Table(name="libros")
 public class Libro {
@@ -20,9 +18,74 @@ public class Libro {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
-    @Transient
-    private List<Autor> autor;
-    private String idioma;
+    //@Transient
+    @ManyToOne
+    private Autor autor;
+    private String idiomas;
+    private int descargas;
 
+    //constructor predeterminado. JPA exige tener este constructor, en todas las entidades.
+    //Este constructor se crea automaticamente mientras no se cree un constructor personalizado. Una vez que se
+    //crea un constructor personalizado, hay que crear este constructor manualmente
+    public Libro(){}
 
+    //constructor personalizado. Recibe un objeto de tipo datosLibro y un objeto de tipo Autor
+    //Se toman los valores de los atributos de esos objetos que se reciben, para asignarlos a los atributos de esta clase
+    //datosLibro.idiomas().get(0) toma el primer idioma de la lista de idiomas
+    public Libro(DatosLibro datosLibro, Autor autor){
+        this.titulo = datosLibro.titulo();
+        this.autor = autor;
+        this.idiomas = datosLibro.idiomas().get(0);
+        this.descargas = datosLibro.descargas();
+    }
+
+    //getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public String getIdiomas() {
+        return idiomas;
+    }
+
+    public void setIdiomas(String idiomas) {
+        this.idiomas = idiomas;
+    }
+
+    public int getDescargas() {
+        return descargas;
+    }
+
+    public void setDescargas(int descargas) {
+        this.descargas = descargas;
+    }
+
+    //Para mostrar la informaciona mapeada
+    @Override
+    public String toString() {
+        return "titulo='" + titulo + '\'' +
+                ", autor=" + autor +
+                ", idiomas=" + idiomas +
+                ", numeroDeDescargas=" + descargas;
+    }
 }
